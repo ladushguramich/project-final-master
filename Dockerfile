bb52f4e40799688d441ca6108bd31f832dd8b732
Lado <maisuradze.vm@gmail.com>
2024-01-30 17:37:05 +0400

    complete settings .env, docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM openjdk:17
+
 WORKDIR /app
 COPY /target/jira-1.0.jar /app/app.jar
+COPY ./resources/* /app/resources/
+
 
 ENV DB_USERNAME=default
 ENV DB_PASSWORD=default
--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -1,12 +1,14 @@
 package com.javarush.jira;
 
 import com.javarush.jira.common.internal.config.AppProperties;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@Log4j2
 @EnableConfigurationProperties(AppProperties.class)
 @EnableCaching
 public class JiraRushApplication {
